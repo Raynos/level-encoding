e51dfda76cc4b1de98c66dc331f52e484ef1f488
Raynos <raynos2@gmail.com>
2012-11-26 22:04:16 -0800

    initial

---
+++ README.md
@@ -0,0 +1,16 @@
+# level-encoding
+
+Encoding functions for leveldb libraries
+
+Basically just the encoding / decoding logic seperated from
+    levelidb.
+
+## Installation
+
+`npm install level-encoding`
+
+## Contributors
+
+ - Raynos
+
+## MIT Licenced
---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+    makeStreamData: require("./makeStreamData")
+    , toBuffer: require("./toBuffer")
+    , toEncoding: require("./toEncoding")
+    , toKeyBuffer: require("./toKeyBuffer")
+    , toKeyEncoding: require("./toKeyEncoding")
+    , toValueBuffer: require("./toValueBuffer")
+    , toValueEncoding: require("./toValueEncoding")
+}
